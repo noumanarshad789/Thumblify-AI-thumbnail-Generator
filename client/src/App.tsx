03c6617f57695a9604f41bfd8224d083b6eb5de9
noumanarshad789 <you@example.commmnoumanarshad456@gmail.com>
2026-01-21 10:39:09 +0500

    Complete home page plus login

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,6 +4,10 @@ import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import "./globals.css";
 import LenisScroll from "./components/LenisScroll";
+import MyGeneration from "./pages/MyGeneration";
+import Generate from "./pages/Generate";
+import YtPreview from "./pages/YtPreview";
+import Login from "./components/Login";
 
 export default function App() {
     return (
@@ -12,6 +16,10 @@ export default function App() {
             <Navbar />
             <Routes>
                 <Route path="/" element={<HomePage />} />
+                <Route path="/my-generations" element={<MyGeneration />} />
+                <Route path="/generate/:id" element={<Generate />} />
+                <Route path="/ytpreview" element={<YtPreview />} />
+                <Route path="/login" element={<Login />} />
             </Routes>
             <Footer />
         </>
